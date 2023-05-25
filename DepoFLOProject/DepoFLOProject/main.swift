8d3b8bd760e962e29562931b531e73d9b816842a
mustafaktaas <mustafa.akts37@gmail.com>
2023-05-25 17:33:06 +0300

    main and malCikar Function Added

--- DepoFLOProject/DepoFLOProject/main.swift
+++ DepoFLOProject/DepoFLOProject/main.swift
@@ -63,4 +63,82 @@ func malEkle() {
     sonIslem = "\(urunAdi) stoğuna toplam \(toplamMiktar) adet \(urunAdi) eklendi."
 }
 
-malEkle()
+
+func malCikar() {
+    let secim = malTuruMenu()
+    guard secim != 4 else { return }
+    
+    var urunKodu = ""
+    var urunAdi = ""
+    
+    switch secim {
+    case 1:
+        urunKodu = "U01"
+        urunAdi = "Ayakkabı"
+    case 2:
+        urunKodu = "U02"
+        urunAdi = "Çanta"
+    case 3:
+        urunKodu = "U03"
+        urunAdi = "Gözlük"
+    default:
+        break
+    }
+    
+    let stokMiktar = stoklar[urunKodu, default: 0]
+    
+    print("\(urunAdi) Çıkartma, Stok Miktarı= \(stokMiktar) adet")
+    print("-------------------------------")
+    
+    var toplamCikartilan = 0
+    var miktar = 0
+    
+    repeat {
+        print("Miktarı Giriniz (çıkış için 0): ", terminator: "")
+        if let giris = readLine(), let miktarInt = Int(giris) {
+            miktar = miktarInt
+        }
+        
+        toplamCikartilan += miktar
+        if toplamCikartilan > stokMiktar {
+            print("Stokta bu kadar yok, \(stokMiktar - (toplamCikartilan - miktar)) adet kaldı...")
+            toplamCikartilan -= miktar
+        }
+    } while miktar != 0
+    
+    stoklar[urunKodu] = max(0, stokMiktar - toplamCikartilan)
+    print("\(urunAdi) stoğuna toplam \(toplamCikartilan) adet \(urunAdi) eklendi.")
+    sonIslem = "\(urunAdi) stoğundan toplam \(toplamCikartilan) adet \(urunAdi) çıkarıldı."
+}
+
+
+func menu() {
+    var secim = 0
+    
+    repeat {
+        print("---- DepoFLO v1.0 ----")
+        print("1- Mal Ekle")
+        print("2- Mal Çıkart")
+        print("3- Çık")
+        print("Seçim: ", terminator: "")
+        
+        if let giris = readLine(), let secimInt = Int(giris) {
+            secim = secimInt
+        }
+        
+        switch secim {
+        case 1:
+            malEkle()
+        case 2:
+            malCikar()
+        case 3:
+            print("Program sonlandırılıyor...")
+        default:
+            print("Geçersiz bir seçim yaptınız.")
+        }
+        
+        print()
+    } while secim != 3
+}
+
+menu()
